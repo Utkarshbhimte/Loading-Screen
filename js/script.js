e7a3721f02f5b503865bcfdb79aa0985dd88e9ff
UtkarshBhimte <utkarshbhimte95@gmail.com>
2016-06-30 15:52:25 +0530

    First Commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$( "document" ).ready(function(){
+
+var myAnimation = anime({
+  targets: 'div',
+  translateX: function() { return anime.random(24, 34) + 'vw'; },
+  scale: function() { return anime.random(10,20) / 10; },
+  rotate: function() { return anime.random(-360,360); },
+  duration: function() { return anime.random(1000,2000); },
+  direction: 'alternate',
+  loop: true,
+});
+
+});
